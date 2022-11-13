a61f86b33b748aa176af0cadd06e2c698730a45d
Laurence Cadag <laurencecads1@gmail.com>
2022-11-13 22:55:44 +0800

    Views folder for router-view

---
+++ src/views/ContactList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ProjectSection />
+  </div>
+</template>
+<script>
+import ProjectSection from '@/components/ProjectSection.vue';
+export default {
+  components: {
+    ProjectSection
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="router-view-div">
+    <Hero />
+    <TechStack />
+  </div>
+</template>
+<script>
+import Hero from '@/components/Hero.vue';
+import TechStack from '@/components/TechStack.vue';
+export default {
+  components: {
+    Hero,
+    TechStack
+  }
+}
+</script>
+<style lang="scss">
+</style>
