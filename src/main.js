16aa08c43b213d12deef3b216abc233a4e329d9f
Laurence Cadag <laurencecads1@gmail.com>
2022-11-26 01:11:13 +0800

    New UI Start: Matrix themed

--- src/main.js
+++ src/main.js
@@ -1,19 +1,50 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'; 
-
-
-
+import { createApp } from "vue";
+import App from "./App.vue";
+// import router from "./router";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 /* import font awesome icon component */
-import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faHtml5, faCss3, faJs, faVuejs, faGit, faSass, faFacebookF, faLinkedinIn, faCodepen, faTwitter, faGithub, faNodeJs, faNode, faPhp} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHtml5,
+  faCss3,
+  faJs,
+  faVuejs,
+  faGit,
+  faSass,
+  faFacebookF,
+  faLinkedinIn,
+  faCodepen,
+  faTwitter,
+  faGithub,
+  faNodeJs,
+  faNode,
+  faPhp,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
-library.add( faHtml5, faCss3, faJs, faVuejs, faGit, faSass, faFacebookF, faLinkedinIn, faCodepen, faTwitter,faBars ,faGithub, faNodeJs, faNode, faPhp)
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+library.add(
+  faHtml5,
+  faCss3,
+  faJs,
+  faVuejs,
+  faGit,
+  faSass,
+  faFacebookF,
+  faLinkedinIn,
+  faCodepen,
+  faTwitter,
+  faBars,
+  faGithub,
+  faNodeJs,
+  faNode,
+  faPhp,
+  faTimesCircle
+);
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
