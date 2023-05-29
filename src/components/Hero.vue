afdb1463fac2c0a29cb718034b076e05b6094dae
Laurence Cadag <laurencecads1@gmail.com>
2023-05-30 00:44:37 +0800

    Hero change styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,13 +4,17 @@
     <div class="hero">
       <h5>Hi, i am</h5>
       <h1>Laurence Cadag</h1>
-      <h2>Vue JS Developer</h2>
+      <h2>Web Developer</h2>
       <h4>
-        I build things for the web using <br />
-        Using Vue JS for fast, easy, <br />
-        and reactive development.
+        Equipped with modern web technologies,<br />
+        we can build the future.
       </h4>
-      <a href="#contact" class="contact cta">Hire Me</a>
+      <a
+      :href="`${publicPath}LAURENCE_CADAG_CV.pdf`"
+      download="LAURENCE_CADAG_CV"
+      class="cta resumeBtn contact"
+      >Download CV</a
+    >
     </div>
   </div>
 </template>
@@ -27,59 +31,64 @@ export default {
 
 <style lang="scss">
 .container {
+  text-align: left;
   width: 100%;
-  min-height: calc(100vh - 100px);
+  margin: 0 auto;
+  min-height: calc(100vh - 200px);
   display: flex;
   align-items: center;
   justify-content: center;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.76), rgba(0, 0, 0, 0.69)),
-    url("../assets/background.jpg");
+  // background-image: linear-gradient(rgba(0, 0, 0, 0.76), rgba(0, 0, 0, 0.69)),
+  //   url("../assets/background.jpg");
   height: calc(100vh - 60px);
   background-repeat: no-repeat;
   background-size: cover;
 
   .hero {
-    width: 90%;
+    width: 80%;
+    padding: 1.5em 1em;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: center;
-    height: 80vh;
+    // height: 80vh;
     margin: 0 auto;
     color: #eee;
 
     h5 {
-      font-size: clamp(1.8rem, 3vw, 2.5rem);
+      font-size: clamp(1.8rem, 2.5vw, 2.5rem);
       font-weight: 300;
       color: #eee;
     }
 
     h1 {
-      font-size: clamp(3.5rem, 5.9vw, 4.8rem);
+      font-size: clamp(2.5rem, 3.5vw, 4.8rem);
       font-weight: 900;
     }
 
     h2 {
       color: $color-accent;
-      font-weight: 500;
-      font-size: clamp(3rem, 4.5vw, 3.5rem);
+      font-weight: 300;
+      font-size: clamp(1.5rem, 3.8vw, 3.5rem);
     }
 
     h4 {
       font-weight: 300;
       margin-bottom: 0.7rem;
       font-size: clamp(1.3rem, 2.5vw, 1.5rem);
+      opacity: .8;
     }
 
     .contact {
-      padding: 1em 1.5em;
-      color: $color-accent;
+      padding: .6em;
+      color: white;
       border-radius: 6px;
-      border: 2px solid rgb(117, 117, 117);
+      border: 2px solid $color-accent;
+      transition: .5s;
     }
 
     .contact:hover {
-      color: $bg-main;
+      color: white;
     }
   }
 }
@@ -87,7 +96,6 @@ export default {
   .container {
     .hero {
       width: 60%;
-      align-items: center;
     }
   }
 }
